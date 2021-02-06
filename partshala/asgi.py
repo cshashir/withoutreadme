abbcb1b637e608be4c1177e4151db39e3e78c6cd
cshashir <cshashir@users.noreply.github.com>
2021-02-06 12:12:26 +0530

    changed to partshala.settings

--- partshala/asgi.py
+++ partshala/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_first.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'partshala.settings')
 
 application = get_asgi_application()
