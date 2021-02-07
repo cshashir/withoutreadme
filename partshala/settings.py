159e27e399a86dee66ad510056f869d15800fabe
cshashir <cshashir@users.noreply.github.com>
2021-02-07 16:54:16 +0530

    css changes again

--- partshala/settings.py
+++ partshala/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(hn+v9*p8c#mt-v^uai1!-0fo7o9156w6pubvnwbjyj_nq&h*='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','www.partshala.co', 'partshala.co', '65.0.132.154', 'ec2-65-0-132-154.ap-south-1.compute.amazonaws.com']
 
