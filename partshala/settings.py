bd369fe2847ff02ff5be8a57733cc14541dca07c
cshashir <71582481+cshashir@users.noreply.github.com>
2021-02-08 14:31:45 +0530

    Update settings.py
        import mimetypes
        mimetypes.add_type("image/svg_xml", ".svg", True)
        mimetypes.add_type("image/svg_xml", ".svgz", True)

--- partshala/settings.py
+++ partshala/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import mimetypes
+mimetypes.add_type("image/svg_xml", ".svg", True)
+mimetypes.add_type("image/svg_xml", ".svgz", True)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
