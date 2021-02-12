5f6bc814f4d7433221b187bbe6ce49e6fc6a459d
cshashir <cshashir@users.noreply.github.com>
2021-02-12 21:57:35 +0530

    DATABASE settings

--- partshala/settings.py
+++ partshala/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'partshala.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'partshaladb-1',
-        'USER' : 'admin',
-        'PASSWORD' : 'Maili1234',
-        'HOST' : 'partshaladb-1.c44sn0xu08up.ap-south-1.rds.amazonaws.com',
-        'PORT' : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'partshaladb-1',
+#         'USER' : 'admin',
+#         'PASSWORD' : 'Maili1234',
+#         'HOST' : 'partshaladb-1.c44sn0xu08up.ap-south-1.rds.amazonaws.com',
+#         'PORT' : '3306',
+#     }
+# }
 
 # declaration for custom User model
 AUTH_USER_MODEL = "users.User"
